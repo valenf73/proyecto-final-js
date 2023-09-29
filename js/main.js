97e174d1803690f3d1f035c50ab053e557696056
Valentín Ferrari <ferrarivalentin73@gmail.com>
2023-09-29 07:05:32 -0300

    Segundo commit - segunda pre-entrega

--- js/main.js
+++ js/main.js
@@ -1,37 +1,56 @@
-// Función que calcula el monto de cada cuota
-function calcularCuotas(monto, cuotas) {
-    if (isNaN(monto) || isNaN(cuotas)) {
-        return "Ingrese valores válidos.";
-    }
+// Variables para almacenar los datos
+let montoTotal = 0;
+let numeroCuotas = 0;
+let interes = 0;
+let mostrarResumen = false;
 
-    if (cuotas < 1) {
-        return "El número de cuotas debe ser mayor a cero.";
-    }
-    
-    let cuotaMensual = monto / cuotas;
-    return "El monto de cada cuota mensual es: $" + cuotaMensual;
-}
+// Array para almacenar los detalles de las cuotas
+let cuotas = [];
 
-// Inicializamos el valor de la variable que vamos a utilizar en el ciclo
-let realizarCalculo = true;
-
-// Ciclo while que permite múltiples cálculos
-while (realizarCalculo) {
-    // Ingreso de datos
-    let montoInput = parseFloat(prompt("Ingrese el monto total:"));
-    let cuotasInput = parseInt(prompt("Ingrese el número de cuotas:"));
-
-    // Llamada de la función cargada en la variable resultado
-    let resultado = calcularCuotas(montoInput, cuotasInput);
-
-    // Visualización del resultado en la consola
-    alert(resultado);
-
-    // Consulta al usuario
-    let opcion = prompt("¿Desea realizar otro cálculo? (s/n)");
+// Función para simular las cuotas
+function simularCuotas() {
+    montoTotal = parseFloat(prompt("Ingrese el monto total:"));
+    numeroCuotas = parseInt(prompt("Ingrese el número de cuotas:"));
+    interes = parseFloat(prompt("Ingrese el interés (%):"));
+    mostrarResumen = confirm("¿Mostrar resumen de cuotas?");
 
     // Verificación
-    if (opcion.toLowerCase() !== "s") {
-        realizarCalculo = false;
+    if (isNaN(montoTotal) || isNaN(numeroCuotas) || isNaN(interes) ||
+        montoTotal <= 0 || numeroCuotas <= 0 || interes < 0) {
+        console.log("Por favor, ingrese valores válidos.");
+        return;
+    }
+
+    // Calcular el monto de cada cuota con intereses
+    const montoCuota = (montoTotal * (1 + (interes / 100))) / numeroCuotas;
+
+    // Limpiar el array de cuotas antes de agregar nuevas
+    cuotas = [];
+
+    // Generar detalles de cuotas
+    for (let i = 1; i <= numeroCuotas; i++) {
+        cuotas.push({
+            numero: i,
+            monto: montoCuota
+        });
+    }
+
+    // Muestra de resumen
+    if (mostrarResumen) {
+        let totalPago = 0;
+        console.log("Resumen de Cuotas:");
+        for (const cuota of cuotas) {
+            console.log(`Cuota ${cuota.numero}: $${cuota.monto.toFixed(2)}`);
+            totalPago += cuota.monto;
+        }
+        console.log(`Total a pagar: $${totalPago.toFixed(2)}`);
+    } else {
+        console.log("Detalles de las Cuotas:");
+        for (const cuota of cuotas) {
+            console.log(`Cuota ${cuota.numero}: $${cuota.monto.toFixed(2)}`);
+        }
     }
 }
+
+// Llamada de la función
+simularCuotas();
